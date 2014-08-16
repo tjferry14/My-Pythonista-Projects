992969c30e158a240f1676c05a906d9ce96d4da7
TJ Ferry <tjtech1014@gmail.com>
2014-08-16 15:40:41 -0400

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,17 +1,23 @@
 import speech
 import ui
-		
+
 v = ui.load_view('speech')
-speech.say('Greetings!', 'en-US', 0.1) 
-textfield = v['textfield1']
-textfield.clear_button_mode = 'while_editing'
+speech.say('Greetings!', 'en-GB', 0.1) 
+textview = v['textview1']
+switch1 = v['switch1']
+
+def brit_switch_action(sender):
+    global lang
+    lang = 'en-GB' if sender.value else 'en-US'
 
 def button_action(sender):
-    text = textfield.text
+    global lang
+    v['switch1'].action = brit_switch_action
+    text = textview.text
     if text == '':
-        speech.say('Please tell me something to say.', 'en-US', 0.1) 
+        speech.say('Please tell me something to say.', lang, 0.1) 
     else:
-        speech.say(text, 'en-US', 0.1)
+        speech.say(text, lang, 0.1) 
 
 button1 = v['button1']
 button1.action = button_action
