aed8df86cc99c50b801f36c2c8330cfb9b213a37
TJ Ferry <tjtech1014@gmail.com>
2014-09-19 19:41:30 -0400

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -14,14 +14,26 @@ def select(sender):
 	v.close()
 	#open_file("~/Notepad" + [sender.selected_row])
 	print (sender, sender.selected_row, sender.items)
-
-def create_file(sender):
-	file_name = v['namefield'].text + '.txt'
-	created_file = v['textview1'].text
+	
+def make_file():
+	global file_name, created_file
 	with open(file_name, 'w') as out_file:
 			out_file.write(created_file)
 	table_data()
 	hud_alert('File successfully created!', 'success', 1.0)
+
+def check(sender):
+	global file_name, created_file
+	file_name = v['namefield'].text + '.txt'
+	if file_name == '.txt':
+		file_name = 'Untitled' + '.txt'
+	else: 
+		pass
+	created_file = v['textview1'].text
+	if created_file == '':
+		hud_alert('No text entered.', 'error', 1.0)
+	else:	
+		make_file()
 
 v = ui.load_view('notepad')
 table_data()
