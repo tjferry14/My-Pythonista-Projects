dc248bb3a96da5750bbf6b98ee48f1e71d5bb56e
TJ Ferry <tjtech1014@gmail.com>
2014-12-12 23:03:14 -0500

    Update and simplify

--- count_text.py
+++ count_text.py
@@ -1,26 +1,9 @@
 from collections import Counter
-from console import hud_alert
-import ui
-
-def check(sender):
-	counttext = v['user_text'].text
-	letter = v['letter'].text
-	if letter == '':
-		hud_alert('Please enter a letter or number to count.', 'error', 1.0)
-	else: 
-		count_text()	
+from console import hud_alert, input_alert
+import dialogs
 	
-def count_text():
-	counttext = v['user_text'].text
-	letter = v['letter'].text
-	counter = Counter(counttext)
-	hud_alert(('There are %s total %s\'s') % (counter[letter], letter), 'success', 3)
+count_text = dialogs.text_dialog(title='Enter text to count', text='')
+letter = input_alert('Counter', 'Enter a letter or number to count', '', 'Count', hide_cancel_button=False)
 
-v = ui.load_view('count_text')
-
-count_button = ui.ButtonItem()
-count_button.title = 'Count'
-count_button.action = check 
-v.right_button_items = [count_button]
-
-v.present('sheet')
+counter = Counter(count_text)
+hud_alert(('There are %s total %s\'s') % (counter[letter], letter), 'success', 3)
