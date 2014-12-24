dec869773573ad9572ed490723bd1aaf301276d0
TJ Ferry <tjtech1014@gmail.com>
2014-12-24 18:47:37 -0500

    Update tweetme.py
    Fix indentation error, don't know how I missed it

--- tweetme.py
+++ tweetme.py
@@ -8,5 +8,5 @@ text = dialogs.text_dialog(title='Tweet a Tweet', autocapitalization=ui.AUTOCAPI
 if len(text) < 140:
 		twitter.post_tweet(account, text, parameters=None)
 		console.hud_alert('Tweet Posted!', 'success', 1.5)
-	else:
+else:
 		console.hud_alert('Exceeded Character Limit', 'error', 1.5)
