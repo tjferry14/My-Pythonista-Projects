f0b6cf3aa44599df701ffa12d5bc49607ad40d26
TJ Ferry <tjtech1014@gmail.com>
2015-09-06 12:08:38 -0400

    Update tweetme.py

--- tweetme.py
+++ tweetme.py
@@ -1,9 +1,7 @@
 import twitter, ui, dialogs, console
-
 all_accounts = twitter.get_all_accounts()
 if len(all_accounts) >= 1:
 	account = all_accounts[0] # get main account
-
 text = dialogs.text_dialog(title='Tweet a Tweet', autocapitalization=ui.AUTOCAPITALIZE_SENTENCES)
 if len(text) < 140:
 		twitter.post_tweet(account, text, parameters=None)
